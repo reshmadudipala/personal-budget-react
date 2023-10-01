48242403bad432a330521ecb592b32b043aeeb70
reshmadudipala <rdudipal@uncc.edu>
2023-10-01 10:20:53 -0800

    Created personal budegt react app

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="bottom">
+        <div class="center">
+            All rights reserved &copy; Reshma Dudipala
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
